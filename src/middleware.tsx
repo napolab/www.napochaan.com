d6e1cd774efd4b985e76f55614b9140e475a6530
naporin0624 <naporin0624@gmail.com>
2023-03-13 03:43:16 +0900

    :+1:

---
+++ src/middleware.tsx
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  const response = NextResponse.next();
+
+  response.headers.set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
+  response.headers.set("X-XSS-Protection", "1; mode=block");
+  response.headers.set("X-Frame-Options", "SAMEORIGIN");
+  response.headers.set("X-Content-Type-Options", "nosniff");
+
+  return response;
+}
