aabad5b0765b0399e556d41aba0b45e8ee0ed51d
naporin0624 <naporin0624@gmail.com>
2023-03-13 01:24:40 +0900

    :+1: add csp setting

--- next.config.js
+++ next.config.js
@@ -10,6 +10,31 @@ const nextConfig = {
     runtime: "experimental-edge",
   },
   pageExtensions: ["tsx"],
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withVanillaExtract(nextConfig);
