d64cec227fd31a7b19d36b0ccb11e876a8a46aaf
naporin0624 <naporin0624@gmail.com>
2023-03-14 01:36:32 +0900

    :+1: remove api route

--- src/pages/api/test.tsx
+++
@@ -1,8 +0,0 @@
-export default function handler() {
-  return new Response(JSON.stringify({ name: "John Doe" }), {
-    status: 200,
-    headers: {
-      "content-type": "application/json",
-    },
-  });
-}
