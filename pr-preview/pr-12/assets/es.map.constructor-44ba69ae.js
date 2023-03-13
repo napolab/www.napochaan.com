500cb2f1ed1935b404032eb1b463898140405a01
naporin0624 <naporin0624@users.noreply.github.com>
2023-03-13 15:29:59 +0000

    Deploy preview for PR 12 🛫

---
+++ pr-preview/pr-12/assets/es.map.constructor-44ba69ae.js
@@ -0,0 +1,2 @@
+var r=Object.defineProperty;var o=(n,t)=>r(n,"name",{value:t,configurable:!0});import{aJ as e,aK as a}from"./iframe-f9e8c9ab.js";var c=e,i=a;c("Map",function(n){return o(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},i);
+//# sourceMappingURL=es.map.constructor-44ba69ae.js.map
