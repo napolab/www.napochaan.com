20ed7ce21bb30ea193963511f4324169eeecb898
naporin0624 <naporin0624@users.noreply.github.com>
2023-03-13 17:04:17 +0000

    Remove preview for PR 12 🛬

--- pr-preview/pr-12/assets/es.number.max-safe-integer-691cd8a4.js
+++
@@ -1,2 +0,0 @@
-import{X as F}from"./iframe-851cc6f9.js";var r=F;r({target:"Number",stat:!0,nonConfigurable:!0,nonWritable:!0},{MAX_SAFE_INTEGER:9007199254740991});
-//# sourceMappingURL=es.number.max-safe-integer-691cd8a4.js.map
