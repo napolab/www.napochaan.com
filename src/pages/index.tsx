25be4ce301e3a1ad1d8a6fe69546e409fae493ce
naporin0624 <naporin0624@gmail.com>
2023-03-16 23:08:25 +0900

    🚧 create first view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import Link from "next/link";
+import Image from "next/image";
 
+import Article from "@components/article";
+import Budoux from "@components/budoux";
 import Heading from "@components/heading";
 import PageHead from "@components/page-head";
 import Section from "@components/section";
-import Switch from "@components/switch";
-import { link } from "@theme/css";
+import characterImage from "public/napochaan.png";
+import Title from "public/title.svg";
 
 import * as styles from "./styles.css";
 
@@ -16,20 +18,55 @@ const Page: NextPage = () => {
       <PageHead title="Home" />
 
       <Section className={styles.pageRoot}>
-        <Heading>aaa</Heading>
-        <Section className={styles.section}>
-          <Heading>bbb</Heading>
-          <Switch aria-label="switch" defaultChecked />
-        </Section>
+        <div className={styles.firstView}>
+          <div className={styles.section1}>
+            <div className={styles.titleRoot}>
+              <Heading unstyled aria-label="napochaan.com" className={styles.pcHeading}>
+                <Title aria-disabled="true" className={styles.pcLogo} />
+              </Heading>
 
-        <p>www.napochaan.com</p>
-        <p>
-          ここは
-          <Link href="https://twitter.com/naporin24690" target="_blank" className={link}>
-            @naporin24690
-          </Link>
-          の WebSite です。
-        </p>
+              <Heading className={styles.spHeading}>napochaan.com</Heading>
+            </div>
+
+            <Image
+              loading="lazy"
+              className={styles.characterImage}
+              src={characterImage}
+              alt="naporitan のオリジナルキャラクター"
+              width={800}
+              height={800}
+            />
+          </div>
+
+          <Article className={styles.aboutMeRoot}>
+            <Heading>About me</Heading>
+            <div className={styles.aboutMe}>
+              <p>
+                <Budoux>こんにちは、naporitan です。</Budoux>
+              </p>
+              <p>
+                <Budoux>
+                  僕はwebアプリケーションエンジニアで、React や TypeScript
+                  を使ったフロントエンドの状態管理を得意としています。
+                </Budoux>
+              </p>
+
+              <p>
+                <Budoux>趣味は音楽を聴くこと、新しい技術に触れること、漫画を読むこと、Valorantをすることです。</Budoux>
+              </p>
+              <p>
+                <Budoux>
+                  好きな言語はTypeScriptとHaskellで、その他にもJavaScript、Python、Ruby、Swiftなど、多様な言語に触れています。
+                </Budoux>
+              </p>
+              <p>
+                <Budoux>
+                  最近は、i18nやa11yといったWebで実現して提供する技術や、最速でユーザーにWebページを表示する技術に興味があります。
+                </Budoux>
+              </p>
+            </div>
+          </Article>
+        </div>
       </Section>
     </>
   );
--- src/pages/styles.css.ts
+++ src/pages/styles.css.ts
@@ -1,22 +1,178 @@
-import { style } from "@vanilla-extract/css";
+import { createContainer, createVar, style } from "@vanilla-extract/css";
+import { calc } from "@vanilla-extract/css-utils";
 
-import { vars } from "@theme/css";
+import { mediaQueries, vars } from "@theme/css";
+
+const characterLayer = createVar();
+const firstViewContainer = createContainer();
 
 /**
  * @package
  */
 export const pageRoot = style({
-  fontFamily: vars.font.notoSansJP,
+  vars: {
+    [characterLayer]: "2",
+  },
+  position: "relative",
+
+  // padding: `0 ${vars.space.lg} ${vars.space.xl}`
+
+  "@media": {
+    [mediaQueries.pc]: {
+      maxWidth: 1920,
+      margin: "0 auto",
+      background: vars.pallets.background.tertiary,
+      borderRadius: vars.borderRadius.lg,
+      overflow: "hidden",
+    },
+  },
 });
 
 /**
  * @package
  */
-export const section = style({
-  height: "100vh",
-  "@supports": {
-    "(height: 100svg)": {
-      height: "100svg",
+export const firstView = style({
+  containerName: firstViewContainer,
+  containerType: "inline-size",
+  position: "relative",
+  width: "100%",
+  isolation: "isolate",
+
+  "@media": {
+    [mediaQueries.pc]: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      alignItems: "flex-end",
+      aspectRatio: "1376 / 1043", // 1440 x 1080 から padding を引いたもの
+      maxHeight: calc.subtract("100vh", calc.multiply(vars.space.lg, 2)),
+
+      "@supports": {
+        "(height: 100svh)": {
+          maxHeight: calc.subtract("100svh", calc.multiply(vars.space.lg, 2)),
+        },
+      },
+    },
+  },
+});
+
+/**
+ * @package
+ */
+export const characterImage = style({
+  userSelect: "none",
+  pointerEvents: "none",
+  zIndex: characterLayer,
+  "@media": {
+    [mediaQueries.pc]: {
+      top: -142,
+      left: -213,
+      position: "absolute",
+      width: "80rem",
+      transform: `rotate(-19deg)`,
+    },
+    [mediaQueries.sp]: {
+      top: 23,
+      left: -123,
+      position: "absolute",
+      width: "36rem",
+      transform: `rotate(-19deg)`,
+      maxInlineSize: "none",
+    },
+  },
+});
+
+/**
+ * @package
+ */
+export const titleRoot = style({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+});
+
+/**
+ * @package
+ */
+export const pcHeading = style({
+  marginTop: "16rem",
+  "@media": {
+    [mediaQueries.sp]: {
+      display: "none",
+    },
+  },
+});
+
+/**
+ * @package
+ */
+export const pcLogo = style({
+  position: "relative",
+  zIndex: 3,
+  height: "4.9rem",
+  marginRight: vars.space.lg,
+});
+
+/**
+ * @package
+ */
+export const spHeading = style({
+  "@media": {
+    [mediaQueries.pc]: {
+      display: "none",
+    },
+  },
+});
+
+export const section1 = style({
+  "@media": {
+    [mediaQueries.sp]: {
+      height: calc.subtract("100vh", vars.space.md),
+      "@supports": {
+        "(height: 100svh)": {
+          height: calc.subtract("100svh", vars.space.md),
+        },
+      },
+    },
+  },
+});
+
+/**
+ * @package
+ */
+export const aboutMeRoot = style({
+  "@media": {
+    [mediaQueries.pc]: {
+      padding: vars.space.lg,
+      width: "40rem",
+      background: `rgba(${vars.pallets.rgb.background.main})`,
+      boxShadow: `0px ${vars.space.sx} ${vars.space.md} 0px ${vars.pallets.background.secondary}`,
+      borderRadius: `${vars.borderRadius.md} 0px 0px ${vars.borderRadius.md}`,
+      zIndex: calc.add(characterLayer, 1),
+    },
+    [mediaQueries.sp]: {
+      padding: vars.space.md,
+      background: `rgba(${vars.pallets.rgb.background.secondary}, 0.9)`,
+      borderRadius: vars.borderRadius.md,
+    },
+  },
+});
+
+/**
+ * @package
+ */
+export const aboutMe = style({
+  display: "flex",
+  flexDirection: "column",
+  fontSize: vars.typography.body,
+  "@media": {
+    [mediaQueries.pc]: {
+      marginTop: "1.5rem",
+      gap: vars.space.md,
+    },
+    [mediaQueries.sp]: {
+      marginTop: "1rem",
+      gap: vars.space.sm,
     },
   },
 });
