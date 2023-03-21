0d276976b7d274c67a2b6ca13734b7f17a1b3005
naporin0624 <naporin0624@users.noreply.github.com>
2023-03-21 19:50:46 +0000

    Deploy preview for PR 17 🛫

---
+++ pr-preview/pr-17/assets/es.number.max-safe-integer-530dbfb1.js
@@ -0,0 +1,2 @@
+import{X as F}from"./iframe-bc04ea8b.js";var r=F;r({target:"Number",stat:!0,nonConfigurable:!0,nonWritable:!0},{MAX_SAFE_INTEGER:9007199254740991});
+//# sourceMappingURL=es.number.max-safe-integer-530dbfb1.js.map
