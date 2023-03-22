3cedc051c8bf0db9b5a35eb3969018bf5b3e038e
naporin0624 <naporin0624@users.noreply.github.com>
2023-03-22 17:18:01 +0000

    Deploy preview for PR 20 🛫

---
+++ pr-preview/pr-20/assets/es.number.max-safe-integer-8581dfe3.js
@@ -0,0 +1,2 @@
+import{X as F}from"./iframe-17e9290f.js";var r=F;r({target:"Number",stat:!0,nonConfigurable:!0,nonWritable:!0},{MAX_SAFE_INTEGER:9007199254740991});
+//# sourceMappingURL=es.number.max-safe-integer-8581dfe3.js.map
