5dbf567c1030dd5cd2f70cc4db86614e3c1b9f48
naporin0624 <32933709+naporin0624@users.noreply.github.com>
2023-03-22 09:04:50 +0000

    Deploying to gh-pages from @ napolab/www.napochaan.com@f75d5f746eb74d27a638c5153210dc3d35fabace 🚀

--- main/assets/es.number.max-safe-integer-e9b14b89.js
+++
@@ -1,2 +0,0 @@
-import{X as F}from"./iframe-83d29aad.js";var r=F;r({target:"Number",stat:!0,nonConfigurable:!0,nonWritable:!0},{MAX_SAFE_INTEGER:9007199254740991});
-//# sourceMappingURL=es.number.max-safe-integer-e9b14b89.js.map
---
+++ main/assets/es.number.max-safe-integer-eeb9cd65.js
@@ -0,0 +1,2 @@
+import{X as F}from"./iframe-a1fa9433.js";var r=F;r({target:"Number",stat:!0,nonConfigurable:!0,nonWritable:!0},{MAX_SAFE_INTEGER:9007199254740991});
+//# sourceMappingURL=es.number.max-safe-integer-eeb9cd65.js.map
