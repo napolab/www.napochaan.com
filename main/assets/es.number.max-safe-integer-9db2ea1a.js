10e49ba879cf062e5b6423203744f631e92b44e5
naporin0624 <32933709+naporin0624@users.noreply.github.com>
2023-03-22 17:24:22 +0000

    Deploying to gh-pages from @ napolab/www.napochaan.com@68913eed60fc15eb4c0cae887be4664b41443ff6 🚀

---
+++ main/assets/es.number.max-safe-integer-9db2ea1a.js
@@ -0,0 +1,2 @@
+import{X as F}from"./iframe-fd751d81.js";var r=F;r({target:"Number",stat:!0,nonConfigurable:!0,nonWritable:!0},{MAX_SAFE_INTEGER:9007199254740991});
+//# sourceMappingURL=es.number.max-safe-integer-9db2ea1a.js.map
