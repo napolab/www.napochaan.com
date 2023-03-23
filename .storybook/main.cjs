ef797b79eb3aed6ed470cbd7f878bae48d6c0b59
naporin0624 <naporin0624@gmail.com>
2023-03-23 17:36:35 +0900

    :+1: change ogp

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -35,12 +35,12 @@ const config = {
       <meta property="og:type" content="website" />
       <meta property="og:title" content="napochaan UI catalog website" />
       <meta property="og:url" content="${baseURL}" />
-      <meta property="og:image" content="${baseURL}/storybook-ogp.png" />
+      <meta property="og:image" content="https://imagedelivery.net/TQ7GECK3x8OMl8rY8WdOxQ/f47aaa2a-5fe7-472e-e928-9d884092da00/ogp" />
       <meta property="og:description" content="UI catalog created by @naporin24690." />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="napochaan UI catalog website" />
-      <meta name="twitter:image" content="${baseURL}/storybook-ogp.png" />
+      <meta name="twitter:image" content="https://imagedelivery.net/TQ7GECK3x8OMl8rY8WdOxQ/f47aaa2a-5fe7-472e-e928-9d884092da00/ogp" />
       <meta name="twitter:description" content="@naporin24690 が作った UI カタログ" />
       <meta name="twitter:site" content="@naporin24690" />
       <meta name="twitter:creator" content="@naporin24690" />
