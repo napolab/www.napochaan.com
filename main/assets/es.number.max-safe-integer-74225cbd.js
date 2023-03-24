e0daa3ceba3a4f858a5654163d30d64df77007b8
naporin0624 <32933709+naporin0624@users.noreply.github.com>
2023-03-24 02:33:26 +0000

    Deploying to gh-pages from @ napolab/www.napochaan.com@8c97f174a836443abd2e9bc0515d3662861e8666 🚀

--- main/assets/es.number.max-safe-integer-74225cbd.js
+++
@@ -1,2 +0,0 @@
-import{X as F}from"./iframe-42317476.js";var r=F;r({target:"Number",stat:!0,nonConfigurable:!0,nonWritable:!0},{MAX_SAFE_INTEGER:9007199254740991});
-//# sourceMappingURL=es.number.max-safe-integer-74225cbd.js.map
---
+++ main/assets/string-d43f1e80.js
@@ -0,0 +1,2 @@
+var i=Object.defineProperty;var e=(t,r)=>i(t,"name",{value:r,configurable:!0});import{X as n}from"./iframe-ac1c39e6.js";var s=n;s({target:"Number",stat:!0},{isNaN:e(function(r){return r!=r},"isNaN")});var f=e(function(r){if(!r)return"";if(typeof r=="string")return r;throw new Error("Description: expected string, got: ".concat(JSON.stringify(r)))},"str");export{f as s};
+//# sourceMappingURL=string-d43f1e80.js.map
