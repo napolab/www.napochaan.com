fc8355bb44978b0aae5e2f0a4eefdad7cac9d308
naporin0624 <naporin0624@users.noreply.github.com>
2023-03-25 17:34:55 +0000

    Remove preview for PR 23 🛬

--- pr-preview/pr-23/assets/es.number.max-safe-integer-5c5b0b32.js
+++
@@ -1,2 +0,0 @@
-import{X as F}from"./iframe-f1a88d1a.js";var r=F;r({target:"Number",stat:!0,nonConfigurable:!0,nonWritable:!0},{MAX_SAFE_INTEGER:9007199254740991});
-//# sourceMappingURL=es.number.max-safe-integer-5c5b0b32.js.map
