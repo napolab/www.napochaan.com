cce8da97a558e0b7b8fd1815860293bf2229615a
naporin0624 <32933709+naporin0624@users.noreply.github.com>
2023-03-26 19:26:04 +0000

    Deploying to gh-pages from @ napolab/www.napochaan.com@380c14f640b6d43b288623b53d54795c05859219 🚀

---
+++ main/assets/es.map.constructor-d497e714.js
@@ -0,0 +1,2 @@
+var r=Object.defineProperty;var o=(n,t)=>r(n,"name",{value:t,configurable:!0});import{aJ as e,aK as a}from"./iframe-4b557e78.js";var c=e,i=a;c("Map",function(n){return o(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},i);
+//# sourceMappingURL=es.map.constructor-d497e714.js.map
