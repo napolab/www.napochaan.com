ecb85427fcbaac7a6703a9123d013c4c7699e8fc
naporin0624 <naporin0624@users.noreply.github.com>
2023-10-31 07:51:21 +0000

    Remove preview for PR 110 🛬

--- pr-preview/pr-110/assets/clsx-58cdbad4.js
+++
@@ -1,2 +0,0 @@
-const i=(...o)=>o.filter(c=>c!==void 0).join(" ");export{i as c};
-//# sourceMappingURL=clsx-58cdbad4.js.map
--- pr-preview/pr-110/assets/wrapped-text.stories-58b5c89e.js
+++
@@ -1,9 +0,0 @@
-import{a as t,j as m}from"./jsx-runtime-03b4ddbf.js";import{r as a}from"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";var _="fotw150";const s=a.forwardRef(({texts:e},i)=>t("span",{className:_,ref:i,children:e.map((o,n)=>m(a.Fragment,{children:[o,e.length===n-1?null:t("wbr",{})]},`${o}__${n}`))})),c=a.memo(s);try{s.displayName="WrappedText",s.__docgenInfo={description:"",displayName:"WrappedText",props:{texts:{defaultValue:null,description:"",name:"texts",required:!0,type:{name:"string[]"}}}}}catch{}const h={title:"components/WrappedText",component:c},u=e=>t("div",{style:{resize:"horizontal",overflow:"scroll",maxWidth:300,padding:16},children:t(c,{...e})}),r=u.bind({});r.args={texts:["Hello","World!"]};var p,l,d;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`args => <div style={{
-  resize: "horizontal",
-  overflow: "scroll",
-  maxWidth: 300,
-  padding: 16
-}}>
-    <WrappedText {...args} />
-  </div>`,...(d=(l=r.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const W=["Default"];export{r as Default,W as __namedExportsOrder,h as default};
-//# sourceMappingURL=wrapped-text.stories-58b5c89e.js.map
