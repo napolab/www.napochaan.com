195e52427b38e48f35bf395b048de4c0a203d201
naporin0624 <naporin0624@gmail.com>
2024-04-02 07:38:37 +0000

    chore: output css

--- src/components/typekit/index.tsx
+++ src/components/typekit/index.tsx
@@ -6,12 +6,9 @@ const ts = Date.now();
 export const OptimizeTypeKit = async ({ projectId }: Props) => {
   console.log("ts", ts);
   const url = `https://use.typekit.net/${projectId}.css?ts=${ts}`;
-  const res = await fetch(url, {
-    next: {
-      revalidate: 3600,
-    },
-  });
+  const res = await fetch(url);
   const css = await res.text();
+  console.log("css", css);
 
   return <style dangerouslySetInnerHTML={{ __html: css }} />;
 };
