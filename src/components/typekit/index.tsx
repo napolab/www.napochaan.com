302e60c160a987bc4ca0d56cd395be50a9befb47
naporin0624 <naporin0624@gmail.com>
2024-04-02 09:13:46 +0000

    chore: remove console

--- src/components/typekit/index.tsx
+++ src/components/typekit/index.tsx
@@ -5,11 +5,9 @@ type Props = {
 
 const ts = Date.now();
 export const OptimizeTypeKit = async ({ projectId }: Props) => {
-  console.log("ts", ts);
   const url = `https://use.typekit.net/${projectId}.css?ts=${ts}`;
   const res = await fetch(url);
   const css = await res.text();
-  console.log("css", css);
 
   return <style dangerouslySetInnerHTML={{ __html: css }} />;
 };
