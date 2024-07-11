8ee854b63d5664bdc9043b5bda382162cbd71e50
naporin0624 <naporin0624@gmail.com>
2024-07-11 02:05:59 +0000

    chore: add sizu.me rss

--- src/adapters/rss/index.ts
+++ src/adapters/rss/index.ts
@@ -1,7 +1,7 @@
 import { XMLParser } from "fast-xml-parser";
 import { unstable_cache as cache } from "next/cache";
 
-import type { ZennRSS } from "./types";
+import type { SizuRSS, ZennRSS } from "./types";
 
 const parser = new XMLParser({
   ignoreAttributes: false,
@@ -18,5 +18,20 @@ export const getZennArticles = cache(
   {
     revalidate: 60 * 60,
     tags: ["zenn", "posts", "all"],
+  },
+);
+
+export const getSizuArticles = cache(
+  async () => {
+    const res = await fetch("https://sizu.me/naporin24690/rss?all=1");
+    const xml = await res.text();
+    const parsed: SizuRSS = parser.parse(xml);
+
+    return parsed.rss.channel.item;
+  },
+  ["sizu-article"],
+  {
+    revalidate: 60 * 60,
+    tags: ["sizume", "posts", "all"],
   },
 );
